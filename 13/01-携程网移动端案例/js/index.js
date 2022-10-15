1e63d282f0045de1d70d4b7c6bd2850c122b6bd7
xyz <xieyinzhangl@gmail.com>
2022-10-15 20:01:16 +0800

    first commit

--- 13/01-携程网移动端案例/js/index.js
+++ 13/01-携程网移动端案例/js/index.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
     var focus = this.document.querySelector('.focus');
     var ul = focus.children[0];
+    var ol = focus.children[1];
     // 获得focus的宽度
     var width = focus.offsetWidth;
     // 利用定时器自动轮播图片
@@ -17,7 +18,7 @@ window.addEventListener('load', function() {
     ul.addEventListener('transitionend', function() {
         // 无缝滚动
         // 如果num走到最后一张图片 快速的跳到第一张
-        if (num == ul.children.length - 2) {
+        if (num >= ul.children.length - 2) {
             num = 0;
             // 去掉过渡效果,让ul快速的跳到目标位置
             ul.style.transition = 'none';
@@ -29,6 +30,11 @@ window.addEventListener('load', function() {
             var translatex = width * num;
             ul.style.transform = 'translateX(' + translatex + 'px)';
         }
+
+        // 小圆点变化
+        ol.querySelector('.current').classList.remove('current');
+        ol.children[num].classList.add('current');
+
     });
 
 })
