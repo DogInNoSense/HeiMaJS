ed39e340fe1be9cb80bfcf341a9187433e847e17
xyz <xieyinzhangl@gmail.com>
2022-10-15 12:37:12 +0800

    first commit

---
+++ 12/animateTop.js
@@ -0,0 +1,24 @@
+function animateTop(obj, target, callback) {
+    // console.log(callback); // callback = function(){} 调用的时候callback()
+    clearInterval(obj.timer);
+    // 改变步长值 (目标值-现在的位置)/10
+    obj.timer = setInterval(function() {
+        // var step = Math.ceil((target - obj.offsetLeft) / 10);
+        var step = (target - window.pageYOffset) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (window.pageYOffset == target) {
+
+            clearInterval(obj.timer);
+            // 回调函数写到计时器里面
+            if (callback) {
+                // 调用回调函数
+                callback();
+            }
+        }
+
+
+        window.scroll(0, window.pageYOffset + step);
+
+
+    }, 15)
+}
